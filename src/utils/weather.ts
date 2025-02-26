fa56dedf6e9e991c6ec1099c07c0280c98d1d3f3
Kalpak43 <111725804+Kalpak43@users.noreply.github.com>
2025-02-26 10:59:01 +0530

    maped images to weather conditions

--- src/utils/weather.ts
+++ src/utils/weather.ts
@@ -4,6 +4,20 @@ const API_KEY = import.meta.env.VITE_WEATHER_KEY;
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 const FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast";
 
+export const weatherBackgrounds = {
+  "clear sky": "url('/clear-sky.webp')",
+  "few clouds": "url('/clouds.webp')",
+  "scattered clouds": "url('/clouds.webp')",
+  "broken clouds": "url('/clouds.webp')",
+  "shower rain": "url('/rain.webp')",
+  rain: "url('/rain.webp')",
+  thunderstorm: "url('/thunderstorm.webp')",
+  snow: "url('/snow.webp')",
+  mist: "url('/haze.webp')",
+  haze: "url('/haze.webp')",
+  default: "url('/clear-sky.webp')", // Default background
+};
+
 export async function getWeather(city: string) {
   const response = await axios.get(
     `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`
